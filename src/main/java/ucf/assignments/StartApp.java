e2fd675583d5a2c0825e9a634d5930c5737f7509
eradney <eradney@knights.ucf.edu>
2021-11-01 19:56:04 -0400

    Setup and UML
    Set up main Java files, still in progress. UML completed

---
+++ src/main/java/ucf/assignments/StartApp.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Emily Radney
+ */
+
+package ucf.assignments;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+
+// Opens app & displays the first screen
+public class StartApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxml
+    }
+}
